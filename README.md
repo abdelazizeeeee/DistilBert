0ae450c8a1b8c19f411140870c5fb241912f822c
abdelazizeeeee <abdelaziz.naija@horizon-tech.tn>
2024-11-06 17:23:37 +0100

    integrate doc scanner model into starter

--- README.md
+++ README.md
@@ -15,14 +15,22 @@ Already a pro? Just edit this README.md and make it your own. Want to make it ea
 
 ```
 cd existing_repo
+<<<<<<< HEAD
 git remote add origin https://gitlab.com/Softylines-Alternance/document_scanner.git
+=======
+git remote add origin https://gitlab.com/Softylines-Alternance/marksheet-ocr.git
+>>>>>>> 04c2584 (integrate doc scanner model into starter)
 git branch -M main
 git push -uf origin main
 ```
 
 ## Integrate with your tools
 
+<<<<<<< HEAD
 - [ ] [Set up project integrations](https://gitlab.com/Softylines-Alternance/document_scanner/-/settings/integrations)
+=======
+- [ ] [Set up project integrations](https://gitlab.com/Softylines-Alternance/fastapi-starter/-/settings/integrations)
+>>>>>>> 04c2584 (integrate doc scanner model into starter)
 
 ## Collaborate with your team
 
---
+++ backend/src/routes/scan.py
@@ -0,0 +1,46 @@
+from fastapi import APIRouter, Depends, HTTPException, status, File, UploadFile
+from fastapi.responses import StreamingResponse
+from ..utils.document_scanner.document_scanner_save_rgb import load_model, scan
+import torch
+import cv2
+import numpy as np
+from io import BytesIO
+
+router = APIRouter()
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model_name = "mbv3"  # or "r50" for ResNet-50
+model = load_model(model_name=model_name, device=device)
+
+
+@router.post("/upload-and-process-image")
+async def upload_and_process_image(file: UploadFile = File(...)):
+    if not file.content_type.startswith("image/"):
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Uploaded file is not an image.",
+        )
+
+    image_bytes = await file.read()
+    image_array = np.frombuffer(image_bytes, np.uint8)
+    image = cv2.imdecode(image_array, cv2.IMREAD_COLOR)
+
+    if image is None:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Could not decode the uploaded image.",
+        )
+
+    processed_image = scan(
+        image=image,
+        trained_model=model,
+        image_size=384,
+        BUFFER=10,
+        threshold=11,
+        c=9
+    )
+
+    _, image_encoded = cv2.imencode(".jpg", processed_image)
+    image_bytes = image_encoded.tobytes()
+
+    return StreamingResponse(BytesIO(image_bytes), media_type="image/jpeg")
