4d3195b2c9cafa65da63d11ac0426e2d5ca9b077
abdelazizeeeee <abdelaziz.naija@horizon-tech.tn>
2024-11-11 11:10:35 +0100

    Change the response to pdf instead of image

--- backend/src/utils/document_scanner/document_scanner_save_rgb.py
+++ backend/src/utils/document_scanner/document_scanner_save_rgb.py
@@ -14,20 +14,25 @@ from torchvision.models.segmentation import (
 
 Mat: TypeAlias = np.ndarray[int, np.dtype[np.generic]]
 
+
 def load_model(num_classes=2, model_name="mbv3", device=torch.device("cpu")):
     """Load the DeepLabV3 model"""
     if model_name == "mbv3":
-        model = deeplabv3_mobilenet_v3_large(num_classes=num_classes, aux_loss=True)
-        checkpoint_path = os.path.join(os.getcwd(), "src/utils/document_scanner/model_mbv3_iou_mix_2C_aux.pth")
+        model = deeplabv3_mobilenet_v3_large(
+            num_classes=num_classes, aux_loss=True)
+        checkpoint_path = os.path.join(
+            os.getcwd(), "src/utils/document_scanner/model_mbv3_iou_mix_2C_aux.pth")
     else:
         model = deeplabv3_resnet50(num_classes=num_classes, aux_loss=True)
-        checkpoint_path = os.path.join(os.getcwd(), "model_r50_iou_mix_2C020.pth")
+        checkpoint_path = os.path.join(
+            os.getcwd(), "model_r50_iou_mix_2C020.pth")
 
     model.to(device)
     checkpoints = torch.load(checkpoint_path, map_location=device)
     model.load_state_dict(checkpoints, strict=False)
     model.eval()
     return model
+
 
 def image_preprocess_transforms(
     mean=(0.4611, 0.4359, 0.3905), std=(0.2193, 0.2150, 0.2109)
@@ -40,45 +45,41 @@ def image_preprocess_transforms(
     )
     return common_transforms
 
+
 def order_points(pts):
     """Rearrange coordinates to order:
     top-left, top-right, bottom-right, bottom-left"""
     rect = np.zeros((4, 2), dtype="float32")
     pts = np.array(pts)
     s = pts.sum(axis=1)
-    # Top-left point will have the smallest sum.
     rect[0] = pts[np.argmin(s)]
-    # Bottom-right point will have the largest sum.
     rect[2] = pts[np.argmax(s)]
 
     diff = np.diff(pts, axis=1)
-    # Top-right point will have the smallest difference.
     rect[1] = pts[np.argmin(diff)]
-    # Bottom-left will have the largest difference.
     rect[3] = pts[np.argmax(diff)]
-    # return the ordered coordinates
     return rect.astype("int").tolist()
+
 
 def find_dest(pts):
     (tl, tr, br, bl) = pts
-    # Finding the maximum width.
     widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
     widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
     maxWidth = max(int(widthA), int(widthB))
 
-    # Finding the maximum height.
     heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
     heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
     maxHeight = max(int(heightA), int(heightB))
-    # Final destination co-ordinates.
-    destination_corners = [[0, 0], [maxWidth, 0], [maxWidth, maxHeight], [0, maxHeight]]
+    destination_corners = [[0, 0], [maxWidth, 0],
+                           [maxWidth, maxHeight], [0, maxHeight]]
 
     return order_points(destination_corners)
+
 
 def scan(image: np.ndarray, trained_model: DeepLabV3, image_size=384, BUFFER=10, threshold=11, c=1):
     """
     Scan and process a document image.
-    
+
     Args:
         image (np.ndarray): Input image as a NumPy array
         trained_model (DeepLabV3): Trained DeepLabV3 model
@@ -86,30 +87,26 @@ def scan(image: np.ndarray, trained_model: DeepLabV3, image_size=384, BUFFER=10,
         BUFFER (int): Buffer for image padding
         threshold (int): Threshold for adaptive thresholding
         c (int): C value for adaptive thresholding
-    
+
     Returns:
         final (numpy.ndarray): Processed document image
     """
-    # Initialize preprocessing transforms
     preprocess_transforms = image_preprocess_transforms()
-    
-    # Check if the input image is valid
+
     if image is None or not isinstance(image, np.ndarray):
         raise ValueError("Input image must be a valid NumPy array.")
-    
-    # Set IMAGE_SIZE and half size for padding calculations
+
     IMAGE_SIZE = image_size
     half = IMAGE_SIZE // 2
 
     imH, imW, C = image.shape
 
-    # Resize image for model processing
-    image_model = cv2.resize(image, (IMAGE_SIZE, IMAGE_SIZE), interpolation=cv2.INTER_NEAREST)
+    image_model = cv2.resize(
+        image, (IMAGE_SIZE, IMAGE_SIZE), interpolation=cv2.INTER_NEAREST)
 
     scale_x = imW / IMAGE_SIZE
     scale_y = imH / IMAGE_SIZE
 
-    # Prepare the image for the model
     image_torch = cast(torch.Tensor, preprocess_transforms(image_model))
     model_input = torch.unsqueeze(image_torch, dim=0)
 
@@ -128,16 +125,17 @@ def scan(image: np.ndarray, trained_model: DeepLabV3, image_size=384, BUFFER=10,
     )
     r_H, r_W = out.shape
 
-    _out_extended = np.zeros((IMAGE_SIZE + r_H, IMAGE_SIZE + r_W), dtype=out.dtype)
-    _out_extended[half : half + IMAGE_SIZE, half : half + IMAGE_SIZE] = out * 255
+    _out_extended = np.zeros(
+        (IMAGE_SIZE + r_H, IMAGE_SIZE + r_W), dtype=out.dtype)
+    _out_extended[half: half + IMAGE_SIZE, half: half + IMAGE_SIZE] = out * 255
     out = _out_extended.copy()
 
     del _out_extended
     gc.collect()
 
-    # Edge Detection.
     canny = cv2.Canny(out.astype(np.uint8), 225, 255)
-    canny = cv2.dilate(canny, cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5)))
+    canny = cv2.dilate(canny, cv2.getStructuringElement(
+        cv2.MORPH_ELLIPSE, (5, 5)))
     contours, _ = cv2.findContours(canny, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
     page = sorted(contours, key=cv2.contourArea, reverse=True)[0]
 
@@ -152,7 +150,7 @@ def scan(image: np.ndarray, trained_model: DeepLabV3, image_size=384, BUFFER=10,
     corners[:, 0] *= scale_x
     corners[:, 1] *= scale_y
 
-    if len(corners) > 0:  # Check if corners are detected
+    if len(corners) > 0:
         print("Corners detected!")
     else:
         print("No corners detected.")
@@ -190,7 +188,7 @@ def scan(image: np.ndarray, trained_model: DeepLabV3, image_size=384, BUFFER=10,
         )
 
         image_extended[
-            top_pad : top_pad + imH, left_pad : left_pad + imW, :
+            top_pad: top_pad + imH, left_pad: left_pad + imW, :
         ] = image
         image_extended = image_extended.astype(np.float32)
 
@@ -215,8 +213,8 @@ def scan(image: np.ndarray, trained_model: DeepLabV3, image_size=384, BUFFER=10,
     )
     final = np.clip(final, a_min=0, a_max=255)
     final = final.astype(np.uint8)
-    
-    return final
+
+    return final, corners
 
 
 def save_image(image: np.ndarray, output_path: str):
@@ -228,15 +226,15 @@ def save_image(image: np.ndarray, output_path: str):
 #     # Set device
 #     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 #     print(f"Using device: {device}")
-    
+
 #     # Load model
 #     model_name = "mbv3"  # or "r50" for ResNet-50
 #     model = load_model(model_name=model_name, device=device)
-    
+
 #     # Process image
-#     input_path = "document-scanner/test_images/IMG_20220721_162811.jpg"  
-#     output_path = "document-scanner/res/res.jpg"  
-    
+#     input_path = "document-scanner/test_images/IMG_20220721_162811.jpg"
+#     output_path = "document-scanner/res/res.jpg"
+
 #     # Scan document with custom parameters
 #     processed_image = scan(
 #         image_path=input_path,
@@ -246,7 +244,7 @@ def save_image(image: np.ndarray, output_path: str):
 #         threshold=11,  # Adjust this value as needed (1-301, odd numbers only)
 #         c=9  # Adjust this value as needed (1-30)
 #     )
-    
+
 #     # Save the processed image
 #     save_image(processed_image, output_path)
 
